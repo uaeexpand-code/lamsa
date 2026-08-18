977acd364ab0557606358ecc76da6e0d2b04c03a
uaeexpand-code <242185344+uaeexpand-code@users.noreply.github.com>
2026-08-18 17:43:06 +0400

    Rename AmadoraApp to LamssaApp

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './AmadoraApp.jsx'
+import App from './LamssaApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
